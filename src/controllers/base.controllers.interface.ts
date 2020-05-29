0a0b22c397b878760e0eb8bd99e595a953ab60e1
paul <paul.18.sma@gmail.com>
2020-05-28 23:37:46 -0300

    changes data type returned by filterBody

--- src/controllers/base.controllers.interface.ts
+++ src/controllers/base.controllers.interface.ts
@@ -2,7 +2,7 @@ import _ from 'lodash';
 export class BaseController {
 
   //(security) filter null values and removes unpermit variables
-  async filterNullValues(obj: any, permit: string[]): Promise<Array<string> | string> {
+  async filterNullValues(obj: any, permit: string[]): Promise<any> {
     const body: any = await _.pickBy(obj, (value, key:string) => {
       return (!_.isNil(value) && _.includes(permit, key));
     });
