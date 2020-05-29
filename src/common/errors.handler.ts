41d1ee26b3e327de1e67e47c3ac03641b9841e3b
paul <paul.18.sma@gmail.com>
2020-05-29 20:54:58 -0300

    improve errors handler

--- src/common/errors.handler.ts
+++ src/common/errors.handler.ts
@@ -1,20 +1,16 @@
 import {Error as MongooseError} from 'mongoose';
-import { httpCodes } from '../config/config';
+import { HttpCodes } from '../config/config';
 
 interface IGenericError {
   property: string;
   message: string;
 }
 
-class MyCustomError {
+// This class permit has one getneric structure of errors as array of error
+class CustomHandlerError {
 
-  private code: number;
-  errors: IGenericError[];
-
-  constructor(){
-    this.code = 500;
-    this.errors = [];
-  }
+  private code: number = HttpCodes.SERVER_ERROR;
+  private errors: IGenericError[] = [];
 
   setCode(code: number){
     this.code = code;
@@ -24,29 +20,48 @@ class MyCustomError {
     return this.code;
   }
 
+  setErrors(error: IGenericError): void{
+    this.errors.push(error);
+  }
+
   getErrors(): IGenericError[]{
     return this.errors;
   }
 }
 
-class ModelError extends Error {
-  constructor(args: any){
-      super(args);
-      this.name = "ModelError"
+// this function mapping every error as defined in, to CustomeHandlerError
+export const errorHandler = (error: any): CustomHandlerError => {
+  const handler = new CustomHandlerError();
+console.log(error, 'from the handler');
+if(error instanceof MongooseError.ValidationError){
+    // mongoose validations
+    handler.setCode(HttpCodes.BAD_REQUEST);
+    Object.keys(error.errors).forEach(prop => {
+      handler.setErrors({property: prop, message: error.errors[prop].message});
+    });
+
+  }else if(error instanceof GenericError){
+    // generic errors
+    handler.setCode(error.code)
+    handler.setErrors({property: error.property, message: error.message});
+
+  }else{
+    // default error
+    handler.setErrors({property: "Server", message: "Server error"});
   }
+
+  return handler;
 }
 
-export const errorHandler = (error: any) => {
-  const myCustonError = new MyCustomError();
+export class GenericError extends Error {
 
-  // catch validation error of mongoose
-  if(error instanceof MongooseError.ValidationError){
-    myCustonError.setCode(httpCodes.BAD_REQUEST);
-    Object.keys(error.errors).forEach(prop => {
-      myCustonError.errors.push({property: prop, message: error.errors[prop].message});
-    });
-  }else{
-    myCustonError.errors.push({property: "Server", message: "Server error"});
+  property: string;
+  code: number;
+
+  constructor(args: any){
+    super(args);
+    this.code = HttpCodes[`${args.type}`];
+    this.property = args.property;
+    this.message = args.message;
   }
-  return myCustonError;
 }
