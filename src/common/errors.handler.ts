9f0baa8812938958c75185ca9e61529149254ae4
paul <paul.18.sma@gmail.com>
2020-05-30 01:35:04 -0300

    handle AccessControllError, emitted when user has not role

--- src/common/errors.handler.ts
+++ src/common/errors.handler.ts
@@ -1,5 +1,8 @@
 import {Error as MongooseError} from 'mongoose';
 import { HttpCodes } from '../config/config';
+import { AccessControlError } from 'role-acl/lib/src/core';
+
+
 
 interface IGenericError {
   property: string;
@@ -32,13 +35,18 @@ class CustomHandlerError {
 // this function mapping every error as defined in, to CustomeHandlerError
 export const errorHandler = (error: any): CustomHandlerError => {
   const handler = new CustomHandlerError();
-console.log(error, 'from the handler');
-if(error instanceof MongooseError.ValidationError){
+
+  if(error instanceof MongooseError.ValidationError){
     // mongoose validations
     handler.setCode(HttpCodes.BAD_REQUEST);
     Object.keys(error.errors).forEach(prop => {
       handler.setErrors({property: prop, message: error.errors[prop].message});
     });
+
+  }else if(error instanceof AccessControlError){
+
+    handler.setCode(HttpCodes.FORBIDDEN)
+    handler.setErrors({property: "Permissions", message: "You have not permissions to perform this action"});
 
   }else if(error instanceof GenericError){
     // generic errors
@@ -47,7 +55,7 @@ if(error instanceof MongooseError.ValidationError){
 
   }else{
     // default error
-    handler.setErrors({property: "Server", message: "Server error"});
+    handler.setErrors({property: "Server", message: "It's not you. It's us. We are having some problems."});
   }
 
   return handler;
