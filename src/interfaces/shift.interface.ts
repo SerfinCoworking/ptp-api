1f3885abf44777d5789174655e308d8e6217e388
paul <paul.18.sma@gmail.com>
2020-08-23 19:15:42 -0300

    removes week and shift modules

--- src/interfaces/shift.interface.ts
+++
@@ -1,8 +0,0 @@
-import { Document } from 'mongoose';
-
-export default interface IShift extends Document {
-  fromDate: Date;
-  toDate: Date;
-  createdAt?: Date;
-  updatedAt?: Date;
-}
--- src/interfaces/week.interface.ts
+++
@@ -1,8 +0,0 @@
-import { Document } from 'mongoose';
-
-export default interface IWeek extends Document {
-  fromDay: number;
-  toDay: number;
-  createdAt?: Date;
-  updatedAt?: Date;
-}
