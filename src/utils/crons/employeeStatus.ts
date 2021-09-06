4af61de5ea6ac3e76c66124b6c9129391b31c0bb
paul <paul.18.sma@gmail.com>
2021-09-05 21:22:45 -0300

    set cronjob to midnight exec

--- src/utils/crons/employeeStatus.ts
+++ src/utils/crons/employeeStatus.ts
@@ -2,7 +2,7 @@ const CronJob = require('cron').CronJob;
 import { cronEmployeeSetBaja } from "../../modules/employeeStatus.module";
 
 
-export const job = new CronJob('* * * * *', () => {
+export const job = new CronJob('0 0 * * *', () => {
   console.log('You will see this message every second');
   cronEmployeeSetBaja();
 });
