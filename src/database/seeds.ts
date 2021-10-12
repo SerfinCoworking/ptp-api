73ede495b4aa1c4d44778146f61c45fdea2eac8a
paul <paul.18.sma@gmail.com>
2021-10-12 20:44:09 -0300

    fix seeds errors

--- src/database/seeds.ts
+++ src/database/seeds.ts
@@ -1,23 +1,23 @@
-import * as db from './dbconfig';
+// import * as db from './dbconfig';
 
-import { createRoles } from './seeds/roles';
-import { createUsers } from './seeds/users';
-import { createEmployees } from './seeds/employees';
-import { createScheduleCalendar } from './seeds/schedules';
-
-
-const seed = async () => {
-  await db.initializeMongo();
-
-  console.log("=========/** STARTING SEEDS... **/========");
-
-  await createRoles();
-  await createUsers();
-  await createEmployees();
-  await createScheduleCalendar();
-
-  console.log("=========/** FINISH SUCCESSFULLY! **/========");
-}
+// import { createRoles } from './seeds/roles';
+// import { createUsers } from './seeds/users';
+// import { createEmployees } from './seeds/employees';
+// import { createScheduleCalendar } from './seeds/schedules';
 
 
-seed();
+// const seed = async () => {
+//   await db.initializeMongo();
+
+//   console.log("=========/** STARTING SEEDS... **/========");
+
+//   await createRoles();
+//   await createUsers();
+//   await createEmployees();
+//   await createScheduleCalendar();
+
+//   console.log("=========/** FINISH SUCCESSFULLY! **/========");
+// }
+
+
+// seed();
